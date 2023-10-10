99df83e4ff18a0a245b084fe6a86a550ab82b22e
DanielFF2 <146964259+DanielFF2@users.noreply.github.com>
2023-10-10 03:52:38 +0100

    Update CutBuffer.java

--- xxl/core/CutBuffer.java
+++ xxl/core/CutBuffer.java
@@ -1,5 +1,18 @@
-package xxl.core;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CutBuffer {
-    
+    private List<Cell> cells;
+
+    public CutBuffer() {
+        cells = new ArrayList<>();
+    }
+
+    public void addCell(Cell cell) {
+        cells.add(cell);
+    }
+
+    public List<Cell> getCells() {
+        return cells;
+    }
 }
